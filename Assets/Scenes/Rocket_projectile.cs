ff8725b85d9c63b70c4da0f9edfcad7befb037cf
Staf dierickx <114059414+StafDierickx@users.noreply.github.com>
2025-11-18 16:04:01 +0100

    quick cleanup

--- Assets/Scenes/Rocket_projectile.cs
+++ Assets/Scenes/Rocket_projectile.cs
@@ -2,24 +2,18 @@ using UnityEngine;
 
 public class Rocket_projectile : MonoBehaviour
 {
-    public float speed = 5;
-    public float radius = 2;
+    public float speed = 20;
+    public float radius = 1;
     public float power = 500;
-
-    void Start()
-    {
-    }
 
     // Update is called once per frame
     void Update()
     {
         transform.position -= transform.TransformDirection(Vector3.forward * speed) * Time.deltaTime;
-        // rb.linearVelocity = transform.forward * (speed * Time.deltaTime);
     }
 
     void OnTriggerEnter(Collider other) 
     {
-        Debug.Log("TOUCHING");
         kickObjects();
         Destroy(gameObject);
     }
